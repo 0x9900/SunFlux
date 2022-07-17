a99a520935c834e537a4df8dba0e02458b1ad810
Fred C <github-fred@hidzz.com>
2022-07-17 12:10:09 -0700

    Improving purge:   * Handles Retries   * Unnecessary context managers removed

--- purge_cluster.py
+++ purge_cluster.py
@@ -19,34 +19,42 @@ logging.basicConfig(
 sqlite3.register_adapter(datetime, adapters.adapt_datetime)
 sqlite3.register_converter('timestamp', adapters.convert_datetime)
 
-def main():
-  config = Config()
-  delta = timedelta(hours=config.get('dxcluster.purge_time', 12))
-  purge_time = datetime.utcnow() - delta
-
-  conn = sqlite3.connect(
-    config['dxcluster.db_name'], timeout=config['dxcluster.db_timeout'],
-    detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES
-  )
-
+def purge(conn, purge_time):
   with conn:
-    curs = conn.cursor()
-    res = curs.execute('select count(*) from dxspot;')
+    res = conn.execute('select count(*) from dxspot;')
     count1 =  res.fetchone()[0]
 
-  with conn:
-    curs = conn.cursor()
-    curs.execute('delete from dxspot where time < ?;', (purge_time,))
+    conn.execute('delete from dxspot where time < ?;', (purge_time,))
     logging.info("Now: %s <=> %s", datetime.utcnow().isoformat(),
                  purge_time.isoformat())
-  with conn:
-    curs = conn.cursor()
-    res = curs.execute('select count(*) from dxspot;')
+
+    res = conn.execute('select count(*) from dxspot;')
     count2 =  res.fetchone()[0]
 
   logging.info('Count %d before delete', count1)
   logging.info('Count %d after delete', count2)
   logging.info('%d records deleted', count1 - count2)
+
+def main():
+  config = Config()
+  delta = timedelta(hours=config.get('dxcluster.purge_time', 12))
+  purge_time = datetime.utcnow() - delta
+
+  logging.info('Database: %s, timeout %d', config['dxcluster.db_name'],
+               config['dxcluster.db_timeout'])
+  conn = sqlite3.connect(
+    config['dxcluster.db_name'], timeout=config['dxcluster.db_timeout'],
+    detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES
+  )
+
+  for retry in range(1, 5):            # number of retries.
+    try:
+      logging.info('Attempt %d', retry)
+      purge(conn, purge_time)
+    except sqlite3.OperationalError:
+      pass
+    else:
+      break
 
 if __name__ == "__main__":
   main()
