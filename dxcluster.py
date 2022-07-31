4b0d8270c9f30229e6d9ffce15896bb51f8ff095
Fred C <github-fred@hidzz.com>
2022-07-31 08:39:40 -0700

    Parse and save WWV Trap database connection errors

--- dxcluster.py
+++ dxcluster.py
@@ -3,6 +3,7 @@
 # (c) 2022 Fred C. (W6BSD)
 #
 # b'DX de SP5NOF:   10136.0  UI5A     FT8 +13dB from KO85 1778Hz   2138Z\r\n'
+# b'WWV de W0MU <18Z> :   SFI=93, A=4, K=2, No Storms -> No Storms\r\n'
 #
 # pylint: disable=no-member,unspecified-encoding
 
@@ -53,6 +54,15 @@ CREATE TABLE IF NOT EXISTS dxspot
 CREATE INDEX IF NOT EXISTS idx_time on dxspot (time DESC);
 CREATE INDEX IF NOT EXISTS idx_de_cont on dxspot (de_cont);
 CREATE INDEX IF NOT EXISTS idx_de_cqzone on dxspot (de_cqzone);
+CREATE TABLE IF NOT EXISTS wwv
+(
+  SFI INTEGER,
+  A INTEGER,
+  K INTEGER,
+  conditions TEXT,
+  time TIMESTAMP
+);
+CREATE INDEX IF NOT EXISTS wwv_idx_time on wwv (time DESC);
 """
 
 TELNET_TIMEOUT = 37
@@ -85,6 +95,7 @@ class DXCCRecord:
   def __repr__(self):
     buffer = ', '.join([f"{f}: {getattr(self, f)}" for f in DXCCRecord.__slots__])
     return f"<DXCCRecord> {buffer}"
+
 
 class DXCC:
 
@@ -173,6 +184,7 @@ def get_band(freq):
   LOG.warning("No band for the frequency %s", freq)
   return 0
 
+
 def login(call, cnx):
   try:
     cnx.expect([b'Please enter your call.*\n'])
@@ -186,15 +198,16 @@ def login(call, cnx):
   cnx.write(b'Set Dx Filter\n')
   cnx.expect(['DX filter.*\n'.encode()])
 
-def decode(line):
+
+def parse_spot(line):
   #
   # DX de DO4DXA-#:  14025.0  GB22GE       CW 10 dB 25 WPM CQ             1516Z
   # 0.........1.........2.........3.........4.........5.........6.........7.........8
   #           0         0         0         0         0         0         0         0
-  if not hasattr(decode, 'dxcc'):
-    decode.dxcc = DXCC()
-  if not line.startswith('DX de'):
-    return None
+  if not hasattr(parse_spot, 'dxcc'):
+    parse_spot.dxcc = DXCC()
+
+  line = line.decode('UTF-8')
   pos = line.index(':')
   try:
     fields = [
@@ -203,8 +216,8 @@ def decode(line):
       line[26:39].rstrip(),
       line[39:70].rstrip(),
     ]
-    call_de = decode.dxcc.lookup(fields[0])
-    call_to = decode.dxcc.lookup(fields[2])
+    call_de = parse_spot.dxcc.lookup(fields[0])
+    call_to = parse_spot.dxcc.lookup(fields[2])
     fields.extend([
       call_de.continent,
       call_to.continent,
@@ -214,22 +227,39 @@ def decode(line):
       call_to.cqzone,
     ])
   except (KeyError, ValueError) as err:
-    LOG.error(err)
+    LOG.warning(err)
     return None
   return Record(fields)
+
+
+def parse_wwv(line):
+  if not hasattr(parse_wwv, 'decoder'):
+    parse_wwv.decoder = re.compile(
+      r'.*\<(?P<Z>\d+)Z\>.*\sSFI=(?P<SFI>\d+), A=(?P<A>\d+), K=(?P<K>\d+), (?P<conditions>.*)$'
+    )
+
+  line = line.decode('UTF-8')
+  _fields = parse_wwv.decoder.match(line.rstrip())
+  if not _fields:
+    return None
+
+  fields = {}
+  for key, val in _fields.groupdict().items():
+    fields[key] = val if key == 'conditions' else int(val)
+  fields['time'] = datetime.utcnow()
+  return fields
+
 
 def read_stream(cdb, cnx):
   expect_exp = [b'DX.*\n', b'WWV de .*\n']
   current = time.time()
   while True:
     code, _, buffer = cnx.expect(expect_exp, timeout=TELNET_TIMEOUT)
-    if code == 0:         # timeout
+    if code == 0:
       current = time.time()
-      buffer = buffer.decode('UTF-8')
-      rec = decode(buffer)
+      rec = parse_spot(buffer)
       if not rec:
         continue
-
       LOG.debug(rec)
       try:
         with cdb:
@@ -242,13 +272,24 @@ def read_stream(cdb, cnx):
       except sqlite3.OperationalError as err:
         LOG.error(err)
     elif code == 1:
-      buffer = buffer.decode('UTF-8')
-      LOG.info(buffer)
-    elif code == -1:
+      fields = parse_wwv(buffer)
+      LOG.info("WWV Fields %s", repr(fields))
+      if not fields:
+        continue
+      try:
+        with cdb:
+          curs = cdb.cursor()
+          curs.execute("INSERT INTO wwv VALUES (?,?,?,?,?)", (
+            fields['SFI'], fields['A'], fields['K'], fields['conditions'],
+            fields['time']
+          ))
+      except sqlite3.OperationalError as err:
+        LOG.error(err)
+    elif code == -1:            # timeout
+      if current < time.time() - 120:
+        break
       LOG.warning('Timeout - sleeping for a few seconds [%s]', cnx.host)
       time.sleep(5)
-    if current < time.time() - 120:
-      break
 
 
 def main():
@@ -273,12 +314,16 @@ def main():
   for server in config['servers']:
     clusters.append(server.split(':'))
 
-  conn = sqlite3.connect(
-    config['db_name'],
-    timeout=config['db_timeout'],
-    detect_types=DETECT_TYPES,
+  try:
+    conn = sqlite3.connect(
+      config['db_name'],
+      timeout=config['db_timeout'],
+      detect_types=DETECT_TYPES,
     isolation_level=None
-  )
+    )
+  except sqlite3.OperationalError as err:
+    LOG.error("Database: %s - %s", config['db_name'], err)
+    return
 
   with conn:
     curs = conn.cursor()
