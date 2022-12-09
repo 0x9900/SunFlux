e230a09a56f3ab1ccfe06afbf9420e74f258b803
Fred C <github-fred@hidzz.com>
2022-12-09 09:34:11 -0800

    Split the outlook graph into 3 separate graphs

--- outlookgraph.py
+++ outlookgraph.py
@@ -26,6 +26,8 @@ plt.style.use(['classic', 'seaborn-talk'])
 
 NOAA_URL = 'https://services.swpc.noaa.gov/text/27-day-outlook.txt'
 
+ALPHA=0.3
+
 class Record(namedtuple("OLRecord", ["Date", "Flux", "AIndex", "KpIndex"])):
   def __new__(cls, items):
     _items = [datetime.strptime(' '.join(items[:3]), "%Y %b %d")]
@@ -67,37 +69,57 @@ class OutLook:
     now = datetime.utcnow().strftime('%Y/%m/%d %H:%M UTC')
 
     plt.rc('ytick', labelsize=12)
-    fig, ax1 = plt.subplots(figsize=(12, 5))
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 5))
+    fig.tight_layout()
     fig.suptitle('27 day Solar Predictions', fontsize=14, fontweight='bold')
     plt.tick_params(labelsize=10)
     fig.autofmt_xdate(rotation=10, ha="center")
 
     # first axis
-    ax1.plot(dates, aindex, ":b", linewidth=1.5, label='A-index')
-    ax1.plot(dates, kindex, "--m", linewidth=1.5, label='KP-index')
-    ax1.set_ylim([0, aindex.max() * 1.15])
-    ax1.set_ylabel('Index')
+    ax1.plot(dates, aindex, linewidth=1.5, color="blue", label='A-index')
+    ax1.set_ylim([1, aindex.max() * 1.15])
     loc = mdates.DayLocator(interval=int(1+len(aindex)/11))
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%a, %b %d UTC'))
-    ax1.xaxis.set_major_locator(loc)
-    ax1.xaxis.set_minor_locator(mdates.DayLocator())
+    ax1.legend(loc='upper right', fontsize="10")
+    ax1.axhspan(0, 5, facecolor='lightgreen', alpha=ALPHA, label='Good')
+    ax1.axhspan(5, 9, facecolor='orange', alpha=ALPHA, label='Ok')
+    ax1.axhspan(9, ax1.get_yticks().max(), facecolor='red', alpha=ALPHA, label='Bad')
+    ax1.grid(color="black", linewidth=.5)
 
-    ax1.grid(color="gray", linestyle="dotted", linewidth=.5)
-    ax1.legend(loc='upper left', fontsize="10")
-
-    # second axis
-    ax2 = ax1.twinx()
-    ax2.plot(dates, flux, "darkgreen", linewidth=1.5, label='Flux')
-    ax2.set_ylim([min(flux) * 0.95, max(flux) * 1.05])
-    ax2.set_ylabel('Flux')
-    ax2.grid(color="green", linestyle="dotted", linewidth=.5)
+    ax2.plot(dates, kindex, linewidth=1.5, color="navy", label='KP-index')
+    ax2.set_ylim([kindex.min() / 1.3, kindex.max() * 1.25])
     ax2.legend(loc='upper right', fontsize="10")
+    ax2.axhspan(0, 3, facecolor='lightgreen', alpha=ALPHA, label='Good')
+    ax2.axhspan(3, 5, facecolor='orange', alpha=ALPHA, label='Ok')
+    ax2.axhspan(5, ax2.get_yticks().max(), facecolor='red', alpha=ALPHA, label='Bad')
+    ax2.grid(color="black", linewidth=.5)
+
+    ax3.plot(dates, flux, "darkgreen", linewidth=1.5, label='Flux')
+    ax3.set_ylim([min(flux)/1.1, max(flux) * 1.05])
+    ax3.legend(loc='upper right', fontsize="10")
+    ax3.axhspan(90, ax3.get_yticks().max(), facecolor='lightgreen', alpha=ALPHA, label='Good')
+    ax3.axhspan(70, 90, facecolor='orange', alpha=ALPHA, label='Ok')
+    ax3.axhspan(40, 70, facecolor='red', alpha=ALPHA, label='Bad')
+    ax3.grid(color="black", linewidth=.5)
+
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter('%a, %b %d UTC'))
+    ax3.xaxis.set_major_locator(loc)
+    ax3.xaxis.set_minor_locator(mdates.DayLocator())
 
     for day in [t.date() for t in dates[:-1]]:
-      if day.isoweekday() == 6:
-        plt.axvspan(mdates.date2num(day), mdates.date2num(day) + 1, color="skyblue", alpha=0.5)
+      if day.isoweekday() != 6:
+        continue
+      for plot in [ax1, ax2, ax3]:
+        plot.axvspan(mdates.date2num(day), mdates.date2num(day) + 1, color="skyblue", alpha=0.5)
 
-    plt.subplots_adjust(bottom=0.20)
+    plt.figtext(0.80, 0.03, "Good", size=12,
+                bbox=dict(boxstyle="round", color='lightgreen', alpha=0.5))
+    plt.figtext(0.87, 0.03, " OK ", size=12,
+                bbox=dict(boxstyle="round", color='orange', alpha=ALPHA))
+    plt.figtext(0.93, 0.03, "Bad", size=12,
+                bbox=dict(boxstyle="round", color='red', alpha=ALPHA))
+
+
+    plt.subplots_adjust(top=0.93, bottom=0.17)
 
     plt.figtext(0.01, 0.02, f'SunFluxBot By W6BSD {now}')
     plt.savefig(filename, transparent=False, dpi=100)
