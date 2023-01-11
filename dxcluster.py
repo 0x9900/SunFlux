b812d90b9fe104912e338e5da94e2548051d5b53
Fred C <github-fred@hidzz.com>
2023-01-10 18:29:44 -0800

    warning -> info

--- dxcluster.py
+++ dxcluster.py
@@ -375,7 +375,7 @@ def read_stream(queue, telnet):
         fields['SFI'], fields['A'], fields['K'], fields['conditions'],
         fields['time'])])
     elif code == 2:
-      LOG.warning('%s Message: %s', telnet.host, buffer)
+      LOG.info('%s Message: %s', telnet.host, buffer)
     elif code == -1:            # timeout
       if current < time.time() - 120:
         break
