b1cb3e17dd0fcbdcf3000fd1c775ec633d348dd1
Fred C <github-fred@hidzz.com>
2023-03-19 18:07:55 -0700

    Minor changes on function calls

--- aindex.py
+++ aindex.py
@@ -35,18 +35,18 @@ GROUP BY dt
 
 WWV_CONDITIONS = "SELECT conditions FROM wwv ORDER BY time DESC LIMIT 1"
 
-def get_conditions(config):
-  conn = sqlite3.connect(config['db_name'], timeout=5,
+def get_conditions(db_name):
+  conn = sqlite3.connect(db_name, timeout=5,
                          detect_types=sqlite3.PARSE_DECLTYPES)
   with conn:
     curs = conn.cursor()
     result = curs.execute(WWV_CONDITIONS).fetchone()
   return result[0]
 
-def get_wwv(config, days):
+def get_wwv(db_name, days):
   data = []
   start_date = datetime.utcnow() - timedelta(days=days)
-  conn = sqlite3.connect(config['db_name'], timeout=5,
+  conn = sqlite3.connect(db_name, timeout=5,
                          detect_types=sqlite3.PARSE_DECLTYPES)
   with conn:
     curs = conn.cursor()
@@ -121,24 +121,25 @@ def graph(data, condition, filename):
   return filename
 
 def main():
+  _config = Config()
+  config = _config.get('aindex')
+  del _config
+
   adapters.install_adapters()
   logging.basicConfig(
     format='%(asctime)s %(name)s:%(lineno)d %(levelname)s - %(message)s', datefmt='%H:%M:%S',
     level=logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO'))
   )
   logger = logging.getLogger('aindex')
-  _config = Config()
-  config = _config.get('aindex')
-  del _config
 
   try:
     name = sys.argv[1]
   except IndexError:
     name = '/tmp/aindex.png'
 
-  data = get_wwv(config, config.get('nb_days', NB_DAYS))
+  data = get_wwv(config['db_name'], config.get('nb_days', NB_DAYS))
   import ipdb; ipdb.set_trace()
-  condition = get_conditions(config)
+  condition = get_conditions(config['db_name'])
   if data:
     graph(data, condition, name)
     logger.info('Graph "%s" saved', name)
