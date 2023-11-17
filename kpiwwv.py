96e0e68dcc18d0d3fef027df30876ab9d5d9adf4
Fred C <github-fred@hidzz.com>
2023-11-16 22:29:32 -0800

    Merge Noaa + local data optimize the creation of the numpy arrays

--- kpiwwv.py
+++ kpiwwv.py
@@ -41,7 +41,7 @@ def bucket(dtm, size=6):
 
 
 def get_conditions(config):
-  db_name = config['showdxcc.db_name']
+  db_name = config['db_name']
   start_time = datetime.utcnow() - timedelta(days=1)
   conn = sqlite3.connect(db_name, timeout=5,
                          detect_types=sqlite3.PARSE_DECLTYPES)
@@ -59,8 +59,10 @@ def get_conditions(config):
 
 def get_kpindex(config):
   data = defaultdict(list)
-  cache_file = config.get('kpiwwv.cache_file', '/tmp/kpiww-noaa.json')
-  cache_time = config.get('kpiwwv.cache_time', 10800)
+  cache_file = config.get('cache_file', '/tmp/kpiww-noaa.json')
+  cache_time = config.get('cache_time', 10800)
+  days = config.get('nb_days', NB_DAYS)
+  start_date = datetime.utcnow() - timedelta(days=days)
   now = time.time()
 
   try:
@@ -77,7 +79,8 @@ def get_kpindex(config):
     try:
       date = datetime.strptime(rec[0], '%Y-%m-%d %H:%M:%S.%f')
       date = date.replace(hour=bucket(date), minute=0, second=0, microsecond=0)
-      data[date].append(float(rec[1]))
+      if date >= start_date:
+        data[date].append(float(rec[1]))
     except ValueError:
       pass
 
@@ -85,11 +88,11 @@ def get_kpindex(config):
 
 
 def get_wwv(config):
-  data = get_kpindex(config)
-  days = config.get('kpiwwv.nb_days', NB_DAYS)
+  data = defaultdict(list)
+  days = config.get('nb_days', NB_DAYS)
   start_date = datetime.utcnow() - timedelta(days=days)
 
-  conn = sqlite3.connect(config['showdxcc.db_name'], timeout=5,
+  conn = sqlite3.connect(config['db_name'], timeout=5,
                          detect_types=sqlite3.PARSE_DECLTYPES)
   with conn:
     curs = conn.cursor()
@@ -99,18 +102,22 @@ def get_wwv(config):
       date = date.replace(hour=bucket(date), minute=0, second=0, microsecond=0)
       data[date].append(elem[1])
 
-  return sorted(data.items())
+  return data
 
 
 def graph(data, condition, filename):
-  datetm = np.array([d[0] for d in data])
-  kimax = np.array([np.max(d[1]) for d in data])
-  kimin = np.array([np.min(d[1]) for d in data])
-  kindex = np.array([np.percentile(d[1], 75) for d in data])
+  values = np.full((len(data), 3), np.nan, dtype=object)
+  for i, row in enumerate(data.values()):
+    row = np.array(row)
+    values[i,:3] = [np.nanmin(row), np.nanmedian(row), np.nanmax(row)]
+
+  key_dates = np.array(list(data.keys())).reshape((-1, 1))
+  data = np.hstack((key_dates, values))
+
   # I should use mpl.colormaps here
   # colors #6efa7b #a7bb36 #aa7f28 #8c4d30 #582a2d
-  colors = ['#6efa7b'] * len(kindex)
-  for pos, val in enumerate(kindex):
+  colors = ['#6efa7b'] * data[:,0].size
+  for pos, val in enumerate(data[:,2]):
     if 4 <= val < 5:
       colors[pos] = '#a7bb36'
     elif 5 <= val < 6:
@@ -126,13 +133,14 @@ def graph(data, condition, filename):
   fig.text(0.01, 0.02, f'SunFluxBot By W6BSD {today}')
   if condition:
     fig.text(0.15, 0.8, "Forecast: " + condition, fontsize=12, zorder=4,
-             bbox=dict(boxstyle='round', linewidth=1, facecolor='linen', alpha=1, pad=.8))
+             bbox={"boxstyle": 'round', "linewidth": 1, "facecolor": 'linen',
+                   "alpha": 1, "pad": .8})
 
   axgc = plt.gca()
   axgc.tick_params(labelsize=10)
-  axgc.bar(datetm, kindex, width=0.20, linewidth=0.75, zorder=2, color=colors)
-  axgc.plot(datetm, kimax, marker='v', linewidth=0, color="green")
-  axgc.plot(datetm, kimin, marker='^', linewidth=0, color="navy")
+  axgc.plot(data[:,0], data[:,1], marker='v', linewidth=0, zorder=3, color="navy")
+  axgc.bar(data[:,0], data[:,2], width=0.20, linewidth=0.75, zorder=2, color=colors)
+  axgc.plot(data[:,0], data[:,3], marker='^', linewidth=0, zorder=4, color="green")
 
   axgc.axhline(y=4, linewidth=1.5, zorder=1.5, color='red')
 
@@ -146,7 +154,7 @@ def graph(data, condition, filename):
   axgc.grid(color="gray", linestyle="dotted", linewidth=.5)
   axgc.margins(.01)
 
-  axgc.legend(['Max', 'Min'], loc='upper right', fontsize='10',
+  axgc.legend(['Min', 'Max'], loc='upper right', fontsize='10',
               facecolor='linen', borderaxespad=1)
 
   fig.autofmt_xdate(rotation=10, ha="center")
@@ -161,13 +169,14 @@ def main():
     level=logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO'))
   )
   logger = logging.getLogger('kpiwwv')
-  config = Config()
+  config = Config().get('kpiwwv')
   try:
     name = sys.argv[1]
   except IndexError:
     name = '/tmp/kpi.png'
 
-  data = get_wwv(config)
+  data = get_kpindex(config)
+  data.update(get_wwv(config))
   condition = get_conditions(config)
   if data:
     graph(data, condition, name)
